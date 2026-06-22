10b27c38c03a67002552e549311279437f2a63bf
maeven-tapa <maeven.tapa@gsfe.tupcavite.edu.ph>
2026-06-22 23:06:59 +0800

    patch 1

--- aquawatch/manage.py
+++ aquawatch/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aquawatch.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aquawatch.aquawatch.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
